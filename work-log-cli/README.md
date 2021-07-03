639dbed50ac0545438d1e4002346cfca4ab35904
Wei Feng <wei.feng@kmart.com.au>
2021-07-03 20:35:43 +1000

    feat: create Quiver note directly!

--- work-log-cli/README.md
+++ work-log-cli/README.md
@@ -1,9 +1,40 @@
-# Quiver Work Log Entry Generator
+# Quiver Work Log Note Generator
+
+This script will create a new "work log" markdown note template in Quiver
+
+## Example
+
+Title: `Z Week 27: 28/6 - 2/7`
+
+```markdown
+## 28/6, Monday
+
+
+## 29/6, Tuesday
+
+
+## 30/6, Wednesday
+
+
+## 1/7, Thursday
+
+
+## 2/7, Friday
+
+
+```
 
 ## Install
 
 ```bash
 npm install -g .
+```
+
+An environment variable `QUIVER_WORKLOG_BASEDIR` is required (can set in `~/.zshrc` for example)
+
+```bash
+# QUIVER
+export QUIVER_WORKLOG_BASEDIR=~/quiver/Work.qvlibrary/92A0B31E-3D46-4D87-B2D9-1567E0622B8D.qvnotebook
 ```
 
 Then it can be run via `work-log` directly
--- work-log-cli/bin/index.js
+++ work-log-cli/bin/index.js
@@ -4,12 +4,51 @@ const dayjs = require('dayjs')
 const weekOfYear = require('dayjs/plugin/weekOfYear')
 dayjs.extend(weekOfYear)
 
-const baseDate = dayjs()
+const uuid = require('uuid')
+const fs = require('fs')
 
-console.log(`Z Week ${baseDate.week()}: ${baseDate.day(1).format('D/M')} - ${baseDate.day(5).format('D/M')}`)
+const baseDir = process.env.QUIVER_WORKLOG_BASEDIR
 
-// Monday to Friday
-for (let i = 1; i < 6; i++) {
-  console.log(baseDate.day(i).format('## D/M, dddd'), '\n\n')
+if (!baseDir) {
+  console.log(`Error: ENV QUIVER_WORKLOG_BASEDIR doesn't exist`)
+  process.exit(1)
 }
 
+const uuidValue = uuid.v4().toUpperCase()
+
+const baseDate = dayjs()
+const timestamp = dayjs().unix()
+
+const title = `Z Week ${baseDate.week()}: ${baseDate.day(1).format('D/M')} - ${baseDate.day(5).format('D/M')}`
+const body = [...Array(5).keys()].map(i => `${baseDate.day(i + 1).format('## D/M, dddd')}\n\n`).join('\n')
+
+// A directory UUID.qvnote
+const newDir = `${baseDir}/${uuidValue}.qvnote`
+fs.mkdirSync(newDir)
+
+// two files:
+// - meta.json
+const meta = {
+  created_at : timestamp,
+  tags: [],
+  title,
+  updated_at: timestamp,
+  uuid: uuidValue
+}
+
+fs.writeFileSync(`${newDir}/meta.json`, JSON.stringify(meta, null, 2))
+
+// - content.json
+const content = {
+  title,
+  cells: [
+    {
+      type: 'markdown',
+      data: body
+    }
+  ]
+}
+
+fs.writeFileSync(`${newDir}/content.json`, JSON.stringify(content, null, 2))
+
+console.log(`new work-log ${title} has been created at ${newDir}\nPlease restart Quiver to take effect`)
